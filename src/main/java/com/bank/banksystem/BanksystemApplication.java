c5f906b1148e206a729d5e9993101d7edd095828
hlia3350 <hlia3350@uni.sydney.edu.au>
2025-09-30 17:21:48 +1000

    initialize

---
+++ src/main/java/com/bank/banksystem/BanksystemApplication.java
@@ -0,0 +1,13 @@
+package com.bank.banksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanksystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/banksystem/BanksystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.banksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanksystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
